c0ade05ab17f956a435a6c20db7f29f5baa7d271
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-06-18 22:29:07 +0100

    Added a pop-up screen when submitting a question in contact.html

--- scripts/contact-script.js
+++ scripts/contact-script.js
@@ -1,22 +1,23 @@
 let formValues = {};
-
+/*
 // Gather data from the form
 function gatherFormData() {
+  formValues.Name = document.getElementById("name").value;
   formValues.Email = document.getElementById("email").value;
   formValues.Message = document.getElementById("text-area").value;
 
-  let output =
-    "Thank you for reaching out, we will get in touch with you shortly!\n\nInformation we received:\n";
+  let output = "Information we received:<br>";
 
   for (key in formValues) {
-    output += `${key} -> ${formValues[key]}\n`;
+    output += `${key} -> ${formValues[key]}<br>`;
   }
 
   return output;
 }
-
+*/
 // Function that clears all values from the fields
 function clearData() {
+  document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("text-area").value = "";
 }
@@ -24,12 +25,24 @@ function clearData() {
 // Function that handles form submittion
 function handleSubmit(e) {
   e.preventDefault();
-  const output = gatherFormData();
+  //const output = gatherFormData();
 
-  alert(output);
+  document.getElementById("body").classList.add("shade");
+  //alert(output);
+
+  //document.getElementById("form-data").innerHTML = output;
+  document.getElementById("pop-up").style.display = "block";
+}
+
+function handleClick() {
+  //document.getElementById("form-data").innerHTML = "";
+  document.getElementById("pop-up").style.display = "none";
 
   clearData();
+  document.getElementById("body").classList.remove("shade");
 }
 
 const form = document.getElementById("leave-a-message");
 form.addEventListener("submit", handleSubmit);
+
+document.getElementById("pop-up-close").addEventListener("click", handleClick);
